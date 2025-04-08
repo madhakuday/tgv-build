d8ae34027af51ceea85a8b5a4aba7707edc1cbe3
Madhak Uday <87520841+madhakuday@users.noreply.github.com>
2025-04-08 19:39:41 +0530

    Delete middlewares directory

--- middlewares/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next); // Pass any caught error to next()
-  };
-  
-module.exports = asyncHandler;
-  
